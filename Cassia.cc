2f640528697885b2f1f3f61efb54b83ad22b5dd8
luki120 <luki120@exerra.xyz>
2024-01-30 05:20:40 -0300

    very much yes

---
+++ Cassia.cc
@@ -0,0 +1,47 @@
+#include <CoreGraphics/CoreGraphics.h>
+#include <objc/message.h>
+
+id(*msg)(id, SEL) = (id (*)(id, SEL)) objc_msgSend;
+id(*msg3)(id, SEL, id) = (id (*)(id, SEL, id)) objc_msgSend;
+id(*msg4)(id, SEL, id, id) = (id (*)(id, SEL, id, id)) objc_msgSend;
+id(*msg5)(id, SEL, CGRect, bool, id) = (id (*)(id, SEL, CGRect, bool, id)) objc_msgSend;
+
+template <typename _Type>
+static inline id msg_with_type(id self, SEL _cmd, _Type type) {
+	id(*msg)(id, SEL, _Type) = (id (*)(id, SEL, _Type)) objc_msgSend;
+	return msg(self, _cmd, type);
+}
+
+static IMP original_IMP;
+
+static void cassia_VDL(id self, SEL _cmd) {
+	original_IMP();
+
+	id _settings = msg_with_type<int>(
+		(id) objc_getClass("_UIBackdropViewSettings"),
+		sel_getUid("settingsForStyle:"),
+		2
+	);
+
+	id _backdrop_view = msg5(
+		msg((id) objc_getClass("_UIBackdropView"), sel_getUid("alloc")),
+		sel_getUid("initWithFrame:autosizesToFitSuperview:settings:"),
+		CGRectZero,
+		true,
+		_settings
+	);
+	msg_with_type<CGFloat>(_backdrop_view, sel_getUid("setAlpha:"), 0.85);
+	msg4(msg(self, sel_getUid("view")), sel_getUid("insertSubview:atIndex:"), _backdrop_view, 0);
+
+}
+
+__attribute__((constructor)) static void init(void) {
+
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		SEL orig_SEL = sel_getUid("viewDidLoad");
+		Method orig_VDL = class_getInstanceMethod(objc_getClass("SBHomeScreenViewController"), orig_SEL);
+		original_IMP = method_setImplementation(orig_VDL, (IMP)cassia_VDL);
+	});
+
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# Cassia
+
+<img src="https://raw.githubusercontent.com/Luki120/luki120.github.io/master/assets/Tweaks/SpringBoard/Cassia/Cassia.png" width="300">
+
+## Features
+
+* Adds a blur to the HomeScreen. This doesn't serve any purpose at all, it's just for showcasing.
+* It doesn't use any Objective-C constructs at all, instead it works by calling C functions & getting everything at runtime.
+
+## Socials
+
+* [Twitter](https://twitter.com/Lukii120)
+
+## Contributing
+
+* Contributions are more than welcomed, but should follow this etiquette:
+
+	* If you're a contributor with write access to this repository, you **should NOT** push to main branch, preferably push to a new one and *then* create the PR.
+	* Keep commit titles short and then explain them in comments or preferably in the commit's description.
+	* Push small commits (e.g if you changed 2 directories, commit one directory, then commit the other one and only THEN push)
+
+## LICENSE
+
+* [Mozilla Public License 2.0](https://www.mozilla.org/en-US/MPL/2.0/)
+
+## Assets LICENSE
+
+* Under no means shall the visual assets of this repository – i.e., all photo-, picto-, icono-, and videographic material – (if any) be altered and/or redistributed for any independent commercial or non-commercial intent beyond its original function in this project. Permissible usage of such content is restricted solely to its express application in this repository and any forks that retain the material in its original, unaltered form only.
